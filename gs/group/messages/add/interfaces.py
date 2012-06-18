05e77b26d4440ffcd3f8c24bd91a183cc827252e
Michael JasonSmith <mpj17@onlinegroups.net>
2012-06-18 12:21:28 +1000

    Adding authentication to the Group Exists page.

--- gs/group/messages/add/interfaces.py
+++ gs/group/messages/add/interfaces.py
@@ -1,10 +1,13 @@
 # coding=utf-8
 from zope.interface.interface import Interface
 from zope.schema import ASCIILine
-# TODO: AuthToken
+from gs.auth.token import AuthToken
 
 class IGSGroupExists(Interface):
+    # TODO: Create a base email-address type
     email = ASCIILine(title=u'Email Address',
                       description=u'The email address to check',
                       required=True)
-    # TODO: Create a base email-address type
+    token = AuthToken(title=u'Token',
+                      description=u'The authentication token',
+                      required=True)
