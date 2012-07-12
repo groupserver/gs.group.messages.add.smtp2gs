a09e687b08b8dac302d3acf5dd10848ee0ec80ee
Michael JasonSmith <mpj17@onlinegroups.net>
2012-07-12 14:55:28 +1000

    Adding the skeleton of smtp2gs.

---
+++ gs/group/messages/add/smtp2gs.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+from argparse import ArgumentParser, FileType, Action
+import sys
+
+def add_post_to_groupserver(url, emailMessage):
+    pass
+
+def MiB_to_B(mb):
+    retval = mb * (2**20)
+    assert retval > mb
+    return retval
+
+def main():
+    p = ArgumentParser(description='Add an email message to GroupServer.',
+                       epilog='Usually %(prog)s is called by a SMTP server '\
+                           '(such as Postfix) in order to add an email '\
+                           'message to a GroupServer group.')
+    p.add_argument('url', metavar='url', 
+                   help='The URL for the GroupServer site.')
+    p.add_argument('-m', '--max-size', dest='maxSize', type=int, default=200,
+                   help='The maximum size of the post that will be accepted, '\
+                       'in mebibyte (default %(default)sMiB).')
+    p.add_argument('-l', '--list', dest='listId',
+                   help='The list to send the message to. By default it is '\
+                       'extracted from the x-original-to header.')
+    p.add_argument('-f', '--file', dest='file', default='-', 
+                   type=FileType('r'),
+                   help='The name of the file that contains the message. If '\
+                       'omitted (or "%(default)s") standard-input will be '\
+                       'read.') 
+    args = p.parse_args()
+    
+
+    emailMessage = args.file.read()
+    l = len(emailMessage)
+    if l == 0:
+        m = '%s: The file containing the email was empty.\n' % p.prog
+        sys.stderr.write(m)
+        sys.exit(10)
+    elif (MiB_to_B(args.maxSize) < l):
+        m = '%s: Email message too large (%d bytes, rather than %d '\
+            'bytes).\n' % (p.prog, len(emailMessage), args.maxSize)
+        sys.stderr.write(m)
+        sys.exit(11)
+    print l
+    add_post_to_groupserver(p.url, emailMessage)
+
+if __name__ == '__main__':
+    main()
